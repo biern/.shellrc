20b98d4624a0efe4b2e905b8c0a0ab264d2d9e6b
Marcin Biernat <mb@marcinbiernat.pl>
2015-05-24 11:32:27 +0200

    split project base name and app name

--- rc.d/functions.sh
+++ rc.d/functions.sh
@@ -1,20 +1,50 @@
+function trb.split_app_base_name {
+    BASE_NAME=${1%/*}
+    APP_NAME=${1#*/}
+
+    if [ -z "$APP_NAME" ] ; then
+        APP_NAME=BASE_NAME
+    fi
+}
+
+
 function trb.venv_dir {
-    echo "$PROJECTS_PATH/$1/venv-$1"
+    trb.split_app_base_name $1
+
+    echo "$PROJECTS_PATH/$BASE_NAME/venv-$APP_NAME"
+}
+
+
+function trb.project_env {
+    trb.split_app_base_name $1
+
+    echo "$PROJECTS_PATH/$BASE_NAME/.$APP_NAME.env"
 }
 
 
 function trb.open_python_project {
-    cd $PROJECTS_PATH/$1/$1
-    source `trb.venv_dir $1`/bin/activate
+    trb.split_app_base_name $1
+
+    PROJECT_DIR=$PROJECTS_PATH/$BASE_NAME/$APP_NAME
+    VENV_DIR=`trb.venv_dir $1`
+
+    if [ -e `trb.project_env $1` ] ; then
+        source `trb.project_env $1`
+    fi
+
+    cd $PROJECT_DIR
+    source $VENV_DIR/bin/activate
 }
 
 
 function trb.create_python_project {
+    trb.split_app_base_name $1
+
     VENV_DIR=`trb.venv_dir $1`
     ALIAS="trb.open_python_project $1"
 
     pushd $PROJECTS_PATH
-    mkdir $1
+    mkdir $BASE_NAME
 
     # Venv
     if [ "$2" == "py2" ] ; then
@@ -24,8 +54,8 @@ function trb.create_python_project {
     fi
 
     # Project dir
-    mkdir $1/$1
-    pushd $1/$1
+    mkdir $BASE_NAME/$APP_NAME
+    pushd $BASE_NAME/$APP_NAME
     git init
 
     echo "alias $1='$ALIAS'" >> $PROJECT_ALIASES_PATH
