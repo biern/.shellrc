83d092df9d01c0f26152f46e5312fe44a95c38ff
Marcin Biernat <mb@marcinbiernat.pl>
2015-05-24 12:44:32 +0200

    install oh-my-zsh if not available

--- zshrc.d/init-oh-my-zsh.zsh
+++ zshrc.d/init-oh-my-zsh.zsh
@@ -1,6 +1,11 @@
 # Path to your oh-my-zsh installation.
 export ZSH=$HOME/.oh-my-zsh
 
+if [ -e $ZSH ] ; then
+else
+   curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+fi
+
 # Set name of the theme to load.
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
