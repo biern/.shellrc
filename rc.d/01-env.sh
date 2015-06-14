96324ef09abb79cf0a6215c8087936b3718d113f
Marcin Biernat <mb@marcinbiernat.pl>
2015-06-14 13:47:54 +0200

    manage HDMI audio / display shortcuts

--- rc.d/01-env.sh
+++ rc.d/01-env.sh
@@ -3,3 +3,9 @@ export ENC_MOUNT_DST="$HOME/enc"
 
 export PROJECTS_PATH="$HOME/projects"
 export PROJECT_ALIASES_PATH="$HOME/.shellrc/rc.d/project_aliases.sh"
+
+export TRB_CONFIG_DIR="$HOME/.config/trb"
+
+export TRB_SETUP_ENV_HDMI_MIXED="MIXED"
+export TRB_SETUP_ENV_HDMI_ONLY="HDMI-ONLY"
+export TRB_SETUP_ENV_HDMI_DEFAULT=$TRB_SETUP_ENV_HDMI_MIXED
--- rc.d/functions.sh
+++ rc.d/functions.sh
@@ -1,3 +1,13 @@
+_TRB_HDMI_CONFIG_FILE=$TRB_CONFIG_DIR/.setup-env.hdmi
+
+
+function trb.init {
+    if [ ! -d $TRB_CONFIG_DIR ] ; then
+        mkdir -p $TRB_CONFIG_DIR
+    fi
+}
+
+
 function trb.project.split_names {
     BASE_NAME=${1%/*}
     APP_NAME=${1#*/}
@@ -90,14 +100,85 @@ function trb.enc.umont {
 }
 
 
+function trb.pulseaudio.change-sink {
+    pacmd set-default-sink $1
+    pacmd list-sink-inputs | grep index | while read line
+    do
+        pacmd move-sink-input `echo $line | cut -f2 -d' '` $1
+    done
+}
+
+
+function trb.kmix.set-master {
+    qdbus org.kde.kmix /Mixers org.kde.KMix.MixSet.setCurrentMaster $TRB_KMIX_OUTPUT $1
+}
+
+
+function trb.audio.use.analog {
+    trb.pulseaudio.change-sink $TRB_PULSEAUDIO_SINK_ANALOG
+    trb.kmix.set-master $TRB_KMIX_CONTROLS_ANALOG
+}
+
+
+function trb.audio.use.hdmi {
+    trb.pulseaudio.change-sink $TRB_PULSEAUDIO_SINK_HDMI
+    trb.kmix.set-master $TRB_KMIX_CONTROLS_HDMI
+}
+
+
 function trb.kmix.master_controls.toggle {
     current=`qdbus org.kde.kmix /Mixers org.kde.KMix.MixSet.currentMasterControl`
-    if [[ $current == $TRB_AUDIO_CONTROLS_HDMI ]] ; then
-        desired=$TRB_AUDIO_CONTROLS_ANALOG
+
+    if [[ $current == $TRB_KMIX_CONTROLS_HDMI ]] ; then
+        trb.audio.use.analog
     else
-        desired=$TRB_AUDIO_CONTROLS_HDMI
+        trb.audio.use.hdmi
     fi
-n
-    qdbus org.kde.kmix /Mixers org.kde.KMix.MixSet.setCurrentMaster $TRB_AUDIO_OUTPUT $desired
+
     qdbus org.kde.kmix /Mixers org.kde.KMix.MixSet.currentMasterControl
+}
+
+
+function trb.setup-env.mixed-laptop-and-hdmi {
+    xrandr --output $TRB_DISPLAY_LAPTOP --auto --primary --output $TRB_DISPLAY_HDMI --auto --right-of $TRB_DISPLAY_LAPTOP
+    trb.audio.use.hdmi
+    echo $TRB_SETUP_ENV_HDMI_MIXED > $_TRB_HDMI_CONFIG_FILE
+}
+
+
+function trb.setup-env.laptop-only {
+    xrandr --output $TRB_DISPLAY_LAPTOP --auto --output $TRB_DISPLAY_HDMI --off
+    trb.audio.use.analog
+}
+
+
+function trb.setup-env.hdmi-only {
+    xrandr --output $TRB_DISPLAY_LAPTOP --off --output $TRB_DISPLAY_HDMI --auto
+    trb.audio.use.hdmi
+    echo $TRB_SETUP_ENV_HDMI_ONLY > $_TRB_HDMI_CONFIG_FILE
+}
+
+
+function trb.setup-env.autodetect {
+    CONNECTED_OUTPUTS=$(xrandr | grep " connected" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
+
+    if [[ $CONNECTED_OUTPUTS =~ "${TRB_DISPLAY_HDMI}" ]] ; then
+        if [ -e $_TRB_HDMI_CONFIG_FILE ] ; then
+            HDMI_ENV=`cat $_TRB_HDMI_CONFIG_FILE`
+        else
+            HDMI_ENV=$TRB_SETUP_ENV_HDMI_DEFAULT
+        fi
+
+        if [[ $HDMI_ENV == $TRB_SETUP_ENV_HDMI_MIXED ]] ; then
+            trb.setup-env.mixed-laptop-and-hdmi
+            notify-send "Switching to mixed display"
+        else
+            trb.setup-env.hdmi-only
+            notify-send "Switching to hdmi only"
+        fi
+
+    else
+        trb.setup-env.laptop-only
+        notify-send "Switching to laptop only"
+    fi
 }
